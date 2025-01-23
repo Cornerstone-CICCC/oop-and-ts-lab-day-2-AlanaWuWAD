230936a413bd7d1853a5138cc1056c75fccce454
Alana <rcclewu@gmail.com>
2025-01-22 21:06:30 -0800

    done

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,16 @@
 import { Component } from "../common/Component.js";
 
 export class AddTodo extends Component {
+  constructor(props){
+    super(props)
+    this.updateTodo = this.updateTodo.bind(this)
+  }
+
+  updateTodo(todo){
+    this.props.todoContext.addTodo(todo)
+
+  }
+
   render() {
     const addElement = document.createElement('div')
     addElement.className = "add-todo"
@@ -8,7 +18,12 @@ export class AddTodo extends Component {
       <input type="text" id="todo-input" placeholder="Enter task details...">
       <button id="todo-add-btn">Add To Do</button>
     `
-
+    addElement.querySelector('#todo-add-btn').addEventListener('click',()=>{
+      const input = addElement.querySelector('#todo-input').value;
+      this.updateTodo(input)
+    })
     return addElement;
   }
+
+ 
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,10 @@
 import { Component } from "../common/Component.js";
 
 export class TodoItem extends Component {
-  render() {
+  render(description) {
     const todoElement = document.createElement('li')
     todoElement.className = "todo-item"
-
+    todoElement.innerHTML = description
     return todoElement;
   }
 }
